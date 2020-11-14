a0803cf91539168ec8c2b181eeba53e1dd6f2c29
Estella24 <2453365519@qq.com>
2020-11-14 10:10:22 +0800

    项目起步-整合ElementUI

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
     <input type="button" value="你点我啦">
     <router-link to="">测试</router-link>
     <router-link to="">深圳特色当当当</router-link>
+    <el-button disabled>默认按钮</el-button>
+  <el-button type="primary" disabled>主要按钮</el-button>
+  <el-button type="success" disabled>成功按钮</el-button>
+  <el-button type="info" disabled>信息按钮</el-button>
+  <el-button type="warning" disabled>警告按钮</el-button>
+  <el-button type="danger" disabled>危险按钮</el-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导包ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//用包ElementUI
+Vue.use(ElementUI);
 //导入抽取的路由,如果导入的是index,直接可以省略
 // import router from './router/index'
 import router from '@/router'
